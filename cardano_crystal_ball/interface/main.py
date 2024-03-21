c77a95616048dd38423a871e2b2f283e8dff48e7
Franz-Hermann Huerkamp <fhh@huerkamp.de>
2024-03-21 15:41:09 +0100

    FIX path to raw_data folder is no programatically found

--- cardano_crystal_ball/interface/main.py
+++ cardano_crystal_ball/interface/main.py
@@ -2,13 +2,15 @@
 # from cardano_crystal_ball.interface.preprocessor_live_coin_data import  get_data_from_api_and_add_date_related_fields
 from preprocessor import    preprocessor
 import pandas as pd
+from cardano_crystal_ball.helper.file_system_helper import search_upwards
+
 
 if __name__ == '__main__':
 
-    start = pd.Timestamp(year=2024,month=1, day=1)
-    end = pd.Timestamp(year=2024,month=1, day=3)
-    csv_fg= '../raw_data/Fear_and_greed_index_5Y.csv'
-    csv_trend= '../raw_data/trends.csv'
+    start = pd.Timestamp(year=2023,month=1, day=1)
+    end = pd.Timestamp(year=2023,month=1, day=3)
+    csv_fg= search_upwards('raw_data')/'raw_data/Fear_and_greed_index_5Y.csv'
+    csv_trend= search_upwards('raw_data')/'raw_data/trends.csv'
     df = preprocessor(start, end, csv_fg, csv_trend)
     # response = api_request(int(start.timestamp()), int(end.timestamp()))
     # response = api_request(int(start.timestamp()), int(end.timestamp()))
