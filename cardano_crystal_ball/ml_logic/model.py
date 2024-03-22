ea55a2ef4a1c36f52fb015ff9344763a6b7ef015
Marcel Knauf <marcel.kn1994@hotmail.de>
2024-03-22 13:15:47 +0100

    updated model.py for double model usage

--- cardano_crystal_ball/ml_logic/model.py
+++ cardano_crystal_ball/ml_logic/model.py
@@ -5,9 +5,10 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from darts import TimeSeries
 from darts.metrics import smape
 
-def initialize_and_compile_model(type_of_model: str, start_learning_rate=0.01, learning_rate_decay=True, batch_size=32, epochs=50, es_patience=7, accelerator="cpu"):
+def initialize_and_compile_model(type_of_model: str, start_learning_rate=0.01, n_rnn_layers=1, learning_rate_decay=True, batch_size=32, epochs=50, early_stopping=True, es_patience=7, accelerator="cpu"):
 
     #for TFT add:   num_attention_heads, lstm_layers, hidden_size   parameters
+
 
     my_stopper = EarlyStopping(
     monitor="val_loss",
@@ -15,9 +16,11 @@ def initialize_and_compile_model(type_of_model: str, start_learning_rate=0.01, l
     min_delta=0.005,
     mode='min',
     )
-
-    pl_trainer_kwargs={"callbacks": [my_stopper],
-                    "accelerator": accelerator}
+    if early_stopping:
+        pl_trainer_kwargs={"callbacks": [my_stopper],
+                        "accelerator": accelerator}
+    else:
+        pl_trainer_kwargs=None
 
     if learning_rate_decay:
         lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau #reduce learning rate when a metric has stopped improving
@@ -32,6 +35,8 @@ def initialize_and_compile_model(type_of_model: str, start_learning_rate=0.01, l
         input_chunk_length=120,
 
         output_chunk_length=24,
+
+        n_rnn_layers = n_rnn_layers,
 
         dropout=0.2,
 
@@ -51,6 +56,7 @@ def initialize_and_compile_model(type_of_model: str, start_learning_rate=0.01, l
         n_epochs=epochs
         )
 
+
     elif type_of_model == "TFT":
         #Code for the TFT model here
         pass
@@ -63,13 +69,24 @@ def initialize_and_compile_model(type_of_model: str, start_learning_rate=0.01, l
 
 def train_model(model, type_of_model: str, y_train: TimeSeries, y_val: TimeSeries, past_covariates: TimeSeries, past_covariates_val: TimeSeries, future_covariates=None, future_covariates_val=None):
 
+    combined_y = y_train.concatenate(y_val)
+    combined_past_covariates = past_covariates.concatenate(past_covariates_val)
 
     if type_of_model == "RNN":
         model.fit(series=y_train,    # the target training data
             past_covariates=past_covariates,     # the multi covariate features training data
             val_series=y_val,  # the target validation data
             val_past_covariates=past_covariates_val,   # the multi covariate features validation data
+            verbose=True)
+
+        n_epochs_model_1 = model.epochs_trained
+
+        model_2 = initialize_and_compile_model("RNN", learning_rate_decay=False, early_stopping=False, epochs=n_epochs_model_1)
+
+        model_2.fit(series=combined_y,    # the target training data
+            past_covariates=combined_past_covariates,     # the multi covariate features training data
             verbose=False)
+
 
     elif type_of_model == "TFT":
         model.fit(series=y_train,    # the target training data
@@ -78,11 +95,19 @@ def train_model(model, type_of_model: str, y_train: TimeSeries, y_val: TimeSerie
             val_past_covariates=past_covariates_val,   # the multi covariate features validation data
             future_covariates=future_covariates,
             val_future_covariates=future_covariates_val,
+            verbose=True)
+
+        n_epochs_model_1 = model.epochs_trained
+
+        model_2 = initialize_and_compile_model("RNN", learning_rate_decay=False, early_stopping=False, epochs=n_epochs_model_1)
+
+        model_2.fit(series=combined_y,    # the target training data
+            past_covariates=combined_past_covariates,     # the multi covariate features training data
             verbose=False)
 
-    print(f"✅ Model trained on {y_train.duration}.")
+    print(f"✅ Model trained on {combined_y.duration}.")
 
-    return model
+    return model_2
 
 
 def evaluate_model(true_series: TimeSeries, forecasted_series: TimeSeries) -> float:
