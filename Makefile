291a2ff7fd52887854828e2bd9ab768cd4c649fb
Shahab Ahmadi <ahmadi.shahabeddin@gmail.com>
2024-03-22 02:14:47 +0100

    modify: makefile and fast.py

--- Makefile
+++ Makefile
@@ -11,12 +11,13 @@ clean_notbookstuff:
 	find . | grep -E "(\.ipynb_checkpoints)" | xargs rm -rvf
 
 pylint:
-  find . -iname "*.py" -not -path "./tests/*" | xargs -n1 -I {}  pylint --output-format=colorized {}; true
+	find . -iname "*.py" -not -path "./tests/*" | xargs -n1 -I {}  pylint --output-format=colorized {}; true
 
 pytest:
   PYTHONDONTWRITEBYTECODE=1 pytest -v --color=yes
 
 run_api:
-	uvicorn cardano_cristall_ball.api.fast:app --reload
+	uvicorn cardano_crystal_ball.api.fast:app --reload
+
 make_preprocessor:
 	python cardano_crystal_ball/interface/main.py
--- cardano_crystal_ball/api/fast.py
+++ cardano_crystal_ball/api/fast.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from cardano_crystal_ball.ml_logic.model import *
 app = FastAPI()
 
+# app.state.model = initialize_and_compile_model('RNN')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
-
-@app.get('/predict')
+@app.get('/')
 def predict():
-    return {'predict': 'test the predict endpoint'}
+
+    return {'prediction': 'comming soon! ;)'}
